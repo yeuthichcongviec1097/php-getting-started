7bc31bea4bcf4b4b0ebcec1345cef99a8efbce95
vinhdragon19971-st <66111026+vinhdragon19971-st@users.noreply.github.com>
2020-07-01 14:15:45 +0700

    a

---
+++ admin_area/includes/db.php
@@ -0,0 +1,5 @@
+<?php 
+
+$con = mysqli_connect("127.0.0.1","baocc","123456","baocc");
+
+?>
---
+++ baocc.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.2
+ */
+
+/**
+ * Database `baocc`
+ */
+
+/* `baocc`.`cart` */
+$cart = array(
+);
+
+/* `baocc`.`categories` */
+$categories = array(
+  array('cat_desc' => 'Loream','cat_id' => '1','cat_title' => 'Bearbrick Basic'),
+  array('cat_desc' => 'Lorem','cat_id' => '2','cat_title' => 'Bearbrick '),
+  array('cat_desc' => 'Lorem','cat_id' => '3','cat_title' => 'Bearbrick Luxury')
+);
+
+/* `baocc`.`customers` */
+$customers = array(
+  array('customer_address' => '1109n njwehfhu','customer_city' => 'ho chi minh','customer_contact' => '0796750305','customer_country' => 'Vietnma','customer_email' => 'bao1552000@gmail.com','customer_id' => '1','customer_image' => 'BR-03-01.jpg','customer_ip' => '::1','customer_name' => 'kendybao','customer_pass' => '123456'),
+  array('customer_address' => 'ưhgkfg2f','customer_city' => 'jbwfhwj','customer_contact' => 'jwehgfufhg','customer_country' => 'jebfkhg','customer_email' => 'baocc@gmail.com','customer_id' => '2','customer_image' => 'BR-02-01.jpg','customer_ip' => '::1','customer_name' => 'CHeo bang','customer_pass' => '123456')
+);
+
+/* `baocc`.`customer_orders` */
+$customer_orders = array(
+  array('customer_id' => '1','due_amount' => '1500','invoice_no' => '1813467165','order_date' => '2020-06-29','order_id' => '0','order_status' => 'pending','qty' => '1','size' => 'Large')
+);
+
+/* `baocc`.`payments` */
+$payments = array(
+);
+
+/* `baocc`.`pending_orders` */
+$pending_orders = array(
+  array('customer_id' => '1','invoice_no' => '1813467165','order_id' => '0','order_status' => '0','product_id' => '1','qty' => '1','size' => 'Large')
+);
+
+/* `baocc`.`products` */
+$products = array(
+  array('cat_id' => '3','date' => '2020-06-25 23:35:25','product_desc' => 'Bearbrick 1000% Jean Michel Basquiat','product_id' => '1','product_img1' => 'BR-01-01.jpg','product_img2' => 'BR-01-02.jpg','product_img3' => 'BR-01-03.jpg','product_keywords' => '12','product_price' => '1500','product_title' => 'Bearbrick 1000% Jean Michel Basquiat','p_cat_id' => '1'),
+  array('cat_id' => '3','date' => '2020-06-25 23:40:58','product_desc' => '<p>Bearbrick Bait Bape 100% /p>','product_id' => '2','product_img1' => 'BR-04-01.jpg','product_img2' => 'BR-04-02.jpg','product_img3' => 'BR-04-03.jpg','product_keywords' => '123','product_price' => '410','product_title' => 'Bearbrick Bait Bape 100%','p_cat_id' => '1'),
+  array('cat_id' => '1','date' => '2020-06-25 23:40:58','product_desc' => '<p>Medicom Taxi Driver: Travis Bickle 1000%</p>','product_id' => '3','product_img1' => 'BR-03-01.jpg','product_img2' => 'BR-03-02.jpg','product_img3' => 'BR-03-03.jpg','product_keywords' => '1234','product_price' => '123','product_title' => 'Medicom Taxi Driver: Travis Bickle 1000% ','p_cat_id' => '1'),
+  array('cat_id' => '2','date' => '2020-06-26 11:18:06','product_desc' => 'Bearbrick 100% WDW Limited Rare Soldout ','product_id' => '5','product_img1' => 'BR-05-01.jpg','product_img2' => 'BR-05-02.jpg','product_img3' => 'BR-05-03.jpg','product_keywords' => '123','product_price' => '50','product_title' => 'Bearbrick 100% WDW Limited Rare Soldout ','p_cat_id' => '1'),
+  array('cat_id' => '1','date' => '2020-06-29 09:04:12','product_desc' => 'Box BearBrick','product_id' => '7','product_img1' => 'BR-06-01.jpg','product_img2' => 'BR-06-02.jpg','product_img3' => 'BR-06-03.jpg','product_keywords' => '123','product_price' => '10','product_title' => 'Box BearBrick','p_cat_id' => '2')
+);
+
+/* `baocc`.`product_categories` */
+$product_categories = array(
+  array('p_cat_desc' => '','p_cat_id' => '1','p_cat_title' => 'Toys'),
+  array('p_cat_desc' => '','p_cat_id' => '2','p_cat_title' => 'Accessories')
+);
+
+/* `baocc`.`slider` */
+$slider = array(
+  array('slide_id' => '1','slide_image' => 'slide-1s.jpg','slide_name' => 'slide-1'),
+  array('slide_id' => '2','slide_image' => 'slide-2s.jpg','slide_name' => 'slide-2s'),
+  array('slide_id' => '3','slide_image' => 'slide-3s.jpg','slide_name' => 'slide-3s'),
+  array('slide_id' => '4','slide_image' => 'slide-4s.jpg','slide_name' => 'slide-4s')
+);
